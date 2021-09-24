e9525fdaf1fd1e0d0dd9a69652f96ecbaf53d8ca
AlexeySvyatov <78646062+AlexeySvyatov@users.noreply.github.com>
2021-09-24 08:42:14 +0300

    Умножение через метод

--- Main.java
+++ Main.java
@@ -1,13 +1,19 @@
 package com.company;
-import java.util.Scanner;
-class Main{
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main{
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Первое число - ");
-        int numb1 = scanner.nextInt();
-        System.out.print("Второе число - ");
-        int numb2 = scanner.nextInt();
-        int numb3 = (int)Math.pow(10, (Math.log10(numb1) + Math.log10(numb2)));
-        System.out.print("Произведение - " + numb3);
+        try(BufferedReader reader = new BufferedReader(new FileReader("multiplication.txt"))){
+            String readed = reader.readLine();
+            String[] txtNumb = readed.split(" multiply ");
+            Multiplication(Integer.parseInt(txtNumb[0]), Integer.parseInt(txtNumb[1]));
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+    public static void Multiplication(int numb1, int numb2){
+        System.out.println("Произвдение равно " + (int)Math.pow(10, (Math.log10(numb1) + Math.log10(numb2))));
     }
 }
